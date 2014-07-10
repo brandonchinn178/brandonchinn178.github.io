58fbc4ff0a554df23db8c96b03e283c6e98df796
Brandon Chinn <brandonchinn178@berkeley.edu>
2014-07-09 19:59:40 -0700

    added scripts directory

--- scripts/nav-projects.js
+++ scripts/nav-projects.js
@@ -1,4 +1,4 @@
-$(document).ready(addProjects());
+$(document).ready(addProjects);
 
 function addProjects() {
     if (window.XMLHttpRequest) {  // code for IE7+, Firefox, Chrome, Opera, Safari
@@ -13,25 +13,15 @@ function addProjects() {
     var projects = xmlDoc.getElementsByTagName("project");
     
     for (i = 0; i < projects.length; i++) {
-        var tr = document.createElement("tr");
-        var td_name = document.createElement("td");
-        var td_img = document.createElement("td");
+        var li = document.createElement("li");
         var a = document.createElement("a");
-        var img = document.createElement("img");
-
         var name = projects[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
         var text = document.createTextNode(name);
         var link = projects[i].getElementsByTagName("link")[0].childNodes[0].nodeValue;
-        var img_name = projects[i].getElementsByTagName("img")[0].childNodes[0].nodeValue;
-
         a.href = link;
         a.target = "_blank";
         a.appendChild(text);
-        img.src = "resources/" + img_name;
-        td_name.appendChild(a);
-        td_img.appendChild(img);
-        tr.appendChild(td_name);
-        tr.appendChild(td_img);
-        document.getElementById("projects").appendChild(tr);
+        li.appendChild(a);
+        document.getElementById("nav-projects").appendChild(li);
     }
 }
