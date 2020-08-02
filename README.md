87aa845611f611539e28a36291204791a2ca55bc
Brandon Chinn <brandonchinn178@gmail.com>
2020-08-01 22:53:11 -0700

    Update quickstart

--- README.md
+++ README.md
@@ -3,8 +3,5 @@
 ## Run locally
 
 ```bash
-brew install ruby
-export PATH="/usr/local/Cellar/ruby/2.6.5/bin:/usr/local/lib/ruby/gems/2.6.0/bin:${PATH}"
-gem install jekyll
-jekyll serve
+./dev.sh
 ```
---
+++ dev.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eux -o pipefail
+
+builtin cd "$(dirname "${BASH_SOURCE[0]}")"
+
+RUBY_PATH=(/usr/local/Cellar/ruby/*/bin)
+
+if [[ ! -d "${RUBY_PATH}" ]]; then
+    echo "Install Ruby with brew" >&2
+    exit 1
+fi
+
+export GEM_PATH=~/.ruby
+export PATH="${GEM_PATH}/bin:${RUBY_PATH}:${PATH}"
+
+gem install --install-dir "${GEM_PATH}" jekyll
+jekyll serve
