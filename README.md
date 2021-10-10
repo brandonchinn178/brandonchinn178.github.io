9ae3b86fcb4729d10a4dba1d26aea430089b187e
Brandon Chinn <brandonchinn178@gmail.com>
2021-10-09 17:52:37 -0700

    Fix dev build

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Run locally
 
 ```bash
+./bundle.sh config set --local path ./vendor/
 ./bundle.sh install
 ./bundle.sh exec jekyll serve
 ```
--- bundle.sh
+++ bundle.sh
@@ -11,6 +11,9 @@ if [[ ! -d "${RUBY_PATH}" ]]; then
     exit 1
 fi
 
+# fix openssl for brew
+export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
+
 export GEM_HOME=~/.ruby
 
 exec "${RUBY_PATH}/bin/bundle" "$@"
