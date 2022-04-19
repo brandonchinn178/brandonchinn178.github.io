c2d4cd29c4c53293f3d79d94b3cb8de4ad0b698c
Brandon Chinn <brandonchinn178@gmail.com>
2022-04-18 21:57:30 -0700

    Update serve.sh

--- serve.sh
+++ serve.sh
@@ -6,8 +6,9 @@ LIVERELOAD_PORT=40004
 JEKYLL_PORT=4000
 
 docker run --rm -it \
+    --name 'gh-page' \
     -v "${PWD}:/srv/jekyll" \
     -p "${LIVERELOAD_PORT}:${LIVERELOAD_PORT}" \
     -p "${JEKYLL_PORT}:4000" \
     jekyll/jekyll \
-    jekyll serve --livereload --livereload-port "${LIVERELOAD_PORT}"
+    jekyll serve --livereload --livereload-port "${LIVERELOAD_PORT}" --future
