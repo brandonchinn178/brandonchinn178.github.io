1ba0281c3bbe97e006a7ac61a40564cf5f21a9ac
Brandon Chinn <brandonchinn178@gmail.com>
2023-05-02 22:05:53 -0700

    Improve dev experience

--- serve.sh
+++ serve.sh
@@ -4,11 +4,17 @@ set -eux -o pipefail
 
 LIVERELOAD_PORT=40004
 JEKYLL_PORT=4000
+CACHE_DIR=/tmp/brandonchinn178-gh-pages-cache
 
-docker run --rm -it \
-    --name 'gh-page' \
-    -v "${PWD}:/srv/jekyll" \
+exec docker run --rm -it \
+    --name 'brandonchinn178-gh-pages' \
     -p "${LIVERELOAD_PORT}:${LIVERELOAD_PORT}" \
     -p "${JEKYLL_PORT}:4000" \
+    --volume="${PWD}:/srv/jekyll:Z" \
+    --volume="${CACHE_DIR}:/usr/gem:Z" \
     jekyll/jekyll \
-    jekyll serve --livereload --livereload-port "${LIVERELOAD_PORT}" --future --source docs/
+    jekyll serve \
+        --livereload \
+        --livereload-port "${LIVERELOAD_PORT}" \
+        --future \
+        --source docs/
